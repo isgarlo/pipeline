1bce6b8ac55fdd6e5180e79e3cb89f3420aa258b
Israel <israel@MacBook-Pro.local>
2018-06-26 09:31:25 +0200

    initial commit

---
+++ test/integration/integration.test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+
+describe('Integration tests', () => {
+
+    it('test 1', () => {
+        expect(1).to.equal(1);
+    });
+    it('test 2', () => {
+        expect(1).to.equal(1);
+    });
+
+});
