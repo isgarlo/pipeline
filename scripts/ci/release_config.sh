44a95d826f0e6fda7b360fde170f09209c3f63d7
Israel <israel@MacBook-Pro.local>
2018-07-13 11:23:35 +0200

    with release config

--- scripts/ci/release_config.sh
+++ scripts/ci/release_config.sh
@@ -4,9 +4,13 @@ echo
 echo Running release_config.sh...
 echo
 
+type=development
 
-if [[ $TRAVIS_TAG != *RC ]]; then
-    echo Making a release
-else
+if [[ $TRAVIS_TAG == *RC ]]; then
     echo Making a release candidate
+else
+    echo Making a release
+    type=prod
 fi
+
+yarn run release
